593fb225adb58775c3762bcff5a0c997c01b064d
eli <1491593447@qq.com>
2023-04-24 03:07:50 +0800

    modify backup_configs.sh

--- 5.backup_configs.sh
+++ 5.backup_configs.sh
@@ -3,13 +3,32 @@
 
 # 声明数组，存储文件路径
 paths=(
-  "./xxx.md"
+  "/etc/pacman.conf"                      # pacman
+  "/etc/sddm.conf.d/sddm.conf"            # sddm
+  "${HOME}/.gitconfig"                    # git
+  "${HOME}/.SpaceVim.d/init.toml"         # spacevim
+  "${HOME}/.config/pgcli/config"          # pgcli
+  "${HOME}/.config/i3/config"             # i3
+  "${HOME}/.zshrc"                        # zsh
+  "${HOME}/.local/share/jrnl/journal.txt" # jrnl
+)
+
+# sudo cp
+sudo_paths=(
+  "/etc/sudoers"                          # pacman
 )
 
 # 使用 for 循环遍历数组，将文件复制到 指定 目录
 for path in "${paths[@]}"
 do
-  cp $path ./cardbox/016-config_box    # 复制文件到 home 目录
+  cp $path ./cardbox/016-config_box    # 复制文件到 指定 目录
+done
+
+for path in "${sudo_paths[@]}"
+do
+  sudo cp $path ./cardbox/016-config_box    # 复制文件到 指定 目录
+  sudo chmod 644 ./cardbox/016-config_box/$(basename $path)   # 修改复制后的文件权限为 644
+  sudo chown eli:eli ./cardbox/016-config_box/$(basename $path) # 修改所属用户和组
 done
 
 echo "备份完成！"
